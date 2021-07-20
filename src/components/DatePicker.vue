4e3b9ec11658c1288379acd594dff8ec9ec7a03a
Mister A <andrew.tarasov1993@gmail.com>
2021-07-20 10:06:12 +0500

    change date picker component

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,22 +1,22 @@
 <template>
-  <v-snackbar v-model="snackbar" multi-line timeout="-1">
+  <v-snackbar value="true" multi-line timeout="-1" max-width="50"  vertical>
     <!-- <template v-slot:action="{ attrs }"> -->
     <v-container>
       <v-row justify='center' style="align-items:center">
-        <v-btn icon color="pink" v-bind="attrs">
+        <v-btn icon color="pink" >
           <v-icon>mdi-chevron-left</v-icon>
         </v-btn>
         Дата
-        <v-btn icon color="pink" v-bind="attrs">
+        <v-btn icon color="pink" >
           <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
       </v-row>
       <v-row justify='center' style="align-items:center">
-        <v-btn icon color="pink" v-bind="attrs">
+        <v-btn icon color="pink" >
           <v-icon>mdi-chevron-left</v-icon>
         </v-btn>
         Время
-        <v-btn icon color="pink" v-bind="attrs">
+        <v-btn icon color="pink">
           <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
       </v-row>
