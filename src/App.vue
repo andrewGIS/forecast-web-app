4c4c017bb31cd35464bf6b178e1a5d060dfb13df
Mister A <andrew.tarasov1993@gmail.com>
2021-07-20 10:05:35 +0500

    add components to app

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,33 @@
       color="primary"
       dark
     >
-
+    Прогноз опасных явлений 
       <v-spacer></v-spacer>
 
     </v-app-bar>
 
     <v-main>
       <Map/>
+      <Options/>
     </v-main>
 
+    <Footer/>
 
   </v-app>
 </template>
 
 <script>
 import Map from './components/Map';
+import Footer from './components/Footer'
+import Options from './components/Options.vue'
 
 export default {
   name: 'App',
 
   components: {
-    Map
+    Map,
+    Footer,
+    Options
   },
 
   data: () => ({
