e47e17329accea2223c9f49e32d2b2ebc1255381
Mister A <andrew.tarasov1993@gmail.com>
2021-07-20 10:04:08 +0500

    add base states in the store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    aviableEventGroup: [{}],
+    isConfigVisible: false,
+    selectedDate: null,
+    selectedModel: 'gfs',
+    selectedForescatType: '00'
   },
   mutations: {
+    SET_CONFIG_VISIBILITY(state, payload){
+      state.isConfigVisible = payload
+    },
+    SET_FORECAST_TYPE(state, payload){
+      state.selectedForescatType = payload
+    },
+    SET_MODEL(state, payload){
+      state.selectedModel = payload
+    }
   },
   actions: {
-  },
-  modules: {
   }
 })
