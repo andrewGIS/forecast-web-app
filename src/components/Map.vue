9a5636bbb821dcff31a436462bbaf80f8742c8bb
Mister A <andrew.tarasov1993@gmail.com>
2021-07-30 00:22:42 +0500

    sample display remote tif

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,8 +1,8 @@
 <template>
   <l-map ref="map">
-    <risk-layer/>
+    <risk-layer />
     <l-tile-layer :url="osmURL"></l-tile-layer>
-    
+
     <!-- <Legend/>
           <Options/>
           <date-picker/> -->
@@ -16,9 +16,9 @@ import { LMap, LTileLayer } from "vue2-leaflet";
 // import DatePicker from './DatePicker.vue'
 import Alert from "./Alert.vue";
 import L from "leaflet";
-import "leaflet-geotiff-2";
+import * as GeoTIFF from "leaflet-geotiff-2";
 import "leaflet-geotiff-2/dist/leaflet-geotiff-plotty"; // requires plotty
-import RiskLayer from "./RiskLayer"
+import RiskLayer from "./RiskLayer";
 
 export default {
   name: "Map",
@@ -36,8 +36,58 @@ export default {
     Alert,
     RiskLayer
   },
-  mounted() {
-    this.$nextTick(() => {
+  methods:{
+    getImagePart() {
+      // Stream reading was stolen from official WEB api docs
+      return fetch(
+        `${process.env.VUE_APP_API_BASE}/get_index`
+      )
+        .then(response => response.body)
+        .then(rb => {
+          const reader = rb.getReader();
+
+          return new ReadableStream({
+            start(controller) {
+              // The following function handles each data chunk
+              function push() {
+                // "done" is a Boolean and value a "Uint8Array"
+                reader.read().then(({ done, value }) => {
+                  // If there is no more data to read
+                  if (done) {
+                    // console.log("done", done);
+                    controller.close();
+                    return;
+                  }
+                  // Get the data and send it to the browser via the controller
+                  controller.enqueue(value);
+                  // Check chunks by logging to the console
+                  //console.log(done, value);
+                  push();
+                });
+              }
+
+              push();
+            }
+          });
+        })
+        .then(stream => {
+          // Respond with our stream
+          return new Response(stream, {
+            headers: { "Content-Type": "image/tif" }
+          }).blob();
+        })
+        .then(result => {
+          // Do things with result
+          //console.log(result);
+          return URL.createObjectURL(result);
+        })
+        .catch(() => {
+          return;
+        });
+    },
+  },
+  async mounted() {
+    this.$nextTick(async() => {
       this.map = this.$refs.map.mapObject; // work as expected
 
       const roptions = {
@@ -49,8 +99,8 @@ export default {
         clampLow: true,
         clampHigh: true,
         // Optional. Plotty color scale used to render the image.
-        //colorScale: "viridis"
-        colorScale: "greys"
+        colorScale: "viridis"
+        //colorScale: "greys"
       };
 
       const renderer = L.LeafletGeotiff.plotty(roptions);
@@ -88,7 +138,7 @@ export default {
         // Optional, override default GeoTIFF function used to load source data
         // Oneof: fromUrl, fromBlob, fromArrayBuffer
         // eslint-disable-next-line no-undef
-        //sourceFunction: GeoTIFF.fromUrl,
+        sourceFunction: GeoTIFF.fromBlob,
 
         // Only required if sourceFunction is GeoTIFF.fromArrayBuffer
         arrayBuffer: null,
@@ -104,39 +154,56 @@ export default {
         noDataKey: undefined,
 
         // The block size to use for buffer
-        blockSize: 65536,
+        //blockSize: 65536,
 
         // Optional, override default opacity of 1 on the image added to the map
         opacity: 1
       };
       // let url = process.env.BASE_URL;
       // eslint-disable-next-line no-unused-vars
-      var layer = L.leafletGeotiff(
-        `http://localhost:8080/temp/gfs.2021051512.003.cape_surface.tif`,
-        options
-      )
 
-      layer.setOpacity(0.5);
+      var layer;
+      let blob = await this.getImagePart()
+      layer = L.leafletGeotiff(
+            blob,
+            options
+          );
+
       layer.addTo(this.map);
 
-      let popup;
-      this.map.on("click", (e) =>{
-        if (!popup) {
-          popup = L.popup()
-            .setLatLng([e.latlng.lat, e.latlng.lng])
-            .openOn(this.map);
-        } else {
-          popup.setLatLng([e.latlng.lat, e.latlng.lng]);
-        }
-        const value = layer.getValueAtLatLng(
-          +e.latlng.lat,
-          +e.latlng.lng
-        );
-        popup
-          .setContent(`Possible value at point (experimental/buggy): ${value}`)
-          .openOn(this.map);
-      });
+      // get by request
+      // fetch(`${process.env.VUE_APP_API_BASE}/get_index`)
+      //   .then(response => response.blob())
+      //   .then(result => {
+      //     console.log(result)
+      //     layer = L.leafletGeotiff(
+      //       // `http://localhost:8080/temp/gfs.2021051512.003.cape_surface.tif`,
+      //       //`${process.env.VUE_APP_API_BASE}/get_index`,
+      //       result,
+      //       options
+      //     );
+      //     layer.addTo(this.map)
+      //   })
+      //   .catch(error => console.log("error", error));
 
+      // sample identification
+      // layer.setOpacity(0.5);
+      // layer.addTo(this.map);
+
+      // let popup;
+      // this.map.on("click", e => {
+      //   if (!popup) {
+      //     popup = L.popup()
+      //       .setLatLng([e.latlng.lat, e.latlng.lng])
+      //       .openOn(this.map);
+      //   } else {
+      //     popup.setLatLng([e.latlng.lat, e.latlng.lng]);
+      //   }
+      //   const value = layer.getValueAtLatLng(+e.latlng.lat, +e.latlng.lng);
+      //   popup
+      //     .setContent(`Possible value at point (experimental/buggy): ${value}`)
+      //     .openOn(this.map);
+      // });
     });
   }
 };
