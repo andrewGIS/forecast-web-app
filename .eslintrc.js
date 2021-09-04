c3aaab58c1d612d6e6f0ec2916c34e7d6ed1600b
Mister A <andrew.tarasov1993@gmail.com>
2021-09-04 14:53:07 +0500

    add base eslint js

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // "env": {
+    //     "browser": true,
+    //     "es2021": true
+    // },
+    "extends": [
+      "eslint:recommended",
+      "plugin:vue/recommended"
+    ],
+    // "parserOptions": {
+    //     "ecmaVersion": 12,
+    //     "sourceType": "module"
+    // },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+      'vue/script-setup-uses-vars': 0
+    }
+};
