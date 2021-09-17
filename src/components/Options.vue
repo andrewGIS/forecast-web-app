c21c1ad21db4a269a7cd3054c3643f7362d3be8b
RinatGIS <rinaha-26@mail.ru>
2021-09-17 17:04:04 +0500

    Add invormation button in memu

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -2,59 +2,106 @@
   <v-snackbar left bottom :value="isVisible" timeout="-1" max-width="100">
     <v-container fluid>
       <v-row justify="end">
-        <v-btn class="mx-4" icon @click="setConfigVisibility(false)">
+        <v-btn class="mx-0" icon @click="setConfigVisibility(false)">
           <v-icon size="24px"> mdi-close </v-icon>
         </v-btn>
       </v-row>
+
+      <v-row class="pa-0"> 
+        <v-col class="pa-0">Модель</v-col>
+      </v-row>
+      <v-row class="pa-0">
+        <v-col cols="10" class="pa-0">
+          <v-select
+            :items="models"
+            v-model="selectedModel"
+            filled
+            label="Модель"
+            dense
+            solo
+          ></v-select>
+        </v-col>
+        <v-col cols="2" class="pa-0">
+          <v-btn class="mx-4" icon>
+          <v-icon size="24px"> mdi-information </v-icon>
+        </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row class="pa-0"> 
+        <v-col class="pa-0">Группа явлений</v-col>
+      </v-row>
+      <v-row class="pa-0">
+        <v-col cols="10" class="pa-0">
+          <v-select
+            label="Выберите группу"
+            :items="eventGroups"
+            item-text="alias"
+            v-model="selectedEvent"
+            filled
+            dense
+            solo
+            return-object
+          ></v-select>
+        </v-col>
+        <v-col cols="2" class="pa-0">
+          <v-btn class="mx-4" icon>
+          <v-icon size="24px"> mdi-information </v-icon>
+        </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row class="pa-0"> 
+        <v-col class="pa-0">Время прогноза</v-col>
+      </v-row>
+      <v-row class="pa-0">
+        <v-col cols="10" class="pa-0">
+          <v-select
+            :items="forecastTypes"
+            v-model="selectedforecastType"
+            filled
+            label="Тип прогноза"
+            dense
+            solo
+          ></v-select>
+        </v-col>
+        <v-col cols="2" class="pa-0">
+          <v-btn class="mx-4" icon>
+          <v-icon size="24px"> mdi-information </v-icon>
+        </v-btn>
+        </v-col>
+      </v-row>
       
-      <v-row>
-        <v-select
-          :items="models"
-          v-model="selectedModel"
-          filled
-          label="Модель"
-          dense
-          solo
-        ></v-select>
+      <v-row class="pa-0"> 
+        <v-col class="pa-0">Индекс</v-col>
       </v-row>
-
-      <v-row>
-        <v-select
-          label="Выберите группу"
-          :items="eventGroups"
-          item-text="alias"
-          v-model="selectedEvent"
-          filled
-          dense
-          solo
-          return-object
-        ></v-select>
-      </v-row>
-
-      <v-row>
-        <v-select
-          :items="forecastTypes"
-          v-model="selectedforecastType"
-          filled
-          label="Тип прогноза"
-          dense
-          solo
-        ></v-select>
-      </v-row>
-
-      <v-row>
-        <v-checkbox
-          @change="indexActive = !indexActive"
-          :checked="indexActive"
-        ></v-checkbox>
-        <v-select          
-          :items="indexList"
-          v-model="selectedIndex"
-          filled
-          dense
-          solo
-          :disabled="!indexActive"
-        ></v-select>
+      <v-row
+        align-content="center"
+        justify="space-between"
+        align="baseline"
+        class="pa-0"
+      >
+        <v-col cols="2" class="pa-0">
+          <v-checkbox
+            @change="indexActive = !indexActive"
+            :checked="indexActive"
+          ></v-checkbox>
+        </v-col>
+        <v-col cols="8" class="pa-0">
+          <v-select
+            :items="indexList"
+            v-model="selectedIndex"
+            filled
+            dense
+            solo
+            :disabled="!indexActive"
+          ></v-select>
+        </v-col>
+        <v-col cols="2" class="pa-0">
+          <v-btn class="mx-4" icon>
+          <v-icon size="24px"> mdi-information </v-icon>
+        </v-btn>
+        </v-col>
       </v-row>
     </v-container>
   </v-snackbar>
@@ -69,15 +116,13 @@ export default {
     models: [],
     eventGroups: [],
     indexList: [],
-    indexActive: false
+    indexActive: false,
   }),
   name: "Options",
-  components: {
-    
-  },
+  components: {},
   computed: {
     ...mapState({
-      isVisible: "isConfigVisible"
+      isVisible: "isConfigVisible",
     }),
     selectedforecastType: {
       get() {
@@ -85,7 +130,7 @@ export default {
       },
       set(value) {
         this.setForecastTypes(value);
-      }
+      },
     },
     selectedModel: {
       get() {
@@ -93,7 +138,7 @@ export default {
       },
       set(value) {
         this.setModel(value);
-      }
+      },
     },
     selectedEvent: {
       get() {
@@ -101,7 +146,7 @@ export default {
       },
       set(value) {
         this.setSelectedEvent(value);
-      }
+      },
     },
     selectedIndex: {
       get() {
@@ -109,7 +154,7 @@ export default {
       },
       set(value) {
         this.setSelectedIndex(value);
-      }
+      },
     },
   },
   methods: {
@@ -118,12 +163,12 @@ export default {
       setForecastTypes: "SET_FORECAST_TYPE",
       setModel: "SET_MODEL",
       setSelectedEvent: "SET_SELECTED_EVENT",
-      setSelectedIndex: "SET_SELECTED_INDEX"
+      setSelectedIndex: "SET_SELECTED_INDEX",
     }),
     getModels() {
       fetch(`${process.env.VUE_APP_API_BASE}/models`)
-        .then(data => data.json())
-        .then(res => {
+        .then((data) => data.json())
+        .then((res) => {
           this.models = res.models;
           this.setModel(res.models[0]);
         })
@@ -133,9 +178,11 @@ export default {
         });
     },
     getGroups() {
-      fetch(`${process.env.VUE_APP_API_BASE}/event_groups?model=${this.selectedModel}`)
-        .then(data => data.json())
-        .then(res => {
+      fetch(
+        `${process.env.VUE_APP_API_BASE}/event_groups?model=${this.selectedModel}`
+      )
+        .then((data) => data.json())
+        .then((res) => {
           this.eventGroups = res.groups;
           this.setSelectedEvent(res.groups[0]);
         })
@@ -145,9 +192,11 @@ export default {
         });
     },
     getIndexes() {
-      fetch(`${process.env.VUE_APP_API_BASE}/indexes?model=${this.selectedModel}`)
-        .then(data => data.json())
-        .then(res => {
+      fetch(
+        `${process.env.VUE_APP_API_BASE}/indexes?model=${this.selectedModel}`
+      )
+        .then((data) => data.json())
+        .then((res) => {
           this.indexList = res.indexes;
           this.setSelectedIndex(res.indexes[0]);
         })
@@ -155,22 +204,21 @@ export default {
           this.indexList = [];
           this.setSelectedIndex(null);
         });
-    }
-
+    },
   },
   watch: {
     selectedModel: function () {
       this.getGroups();
       this.getIndexes();
-    }
+    },
   },
   mounted() {
     this.getGroups();
     this.getModels();
     this.getIndexes();
-  }
+  },
 };
 </script>
 
-<style>
+<style >
 </style>
