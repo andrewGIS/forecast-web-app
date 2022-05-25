d912185723d8d431ce77eb5412b4280b5e0bebd0
Mister A <andrew.tarasov1993@gmail.com>
2022-05-25 22:13:07 +0500

    fix: axios config for sending csrf token

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,30 @@
 import axios from 'axios'
 //import store from '../store'
+// TODO разобраться с авторизованными запросами и нет
+// TODO все запросы сделать через клиента
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
 
 const client = axios.create({
   baseURL: process.env.VUE_APP_API_BASE,
   headers: {
     'Content-Type': 'application/json',
   },
-  //withCredentials: true,
+  withCredentials: true,
 })
 
-// client.interceptors.response.use(null, (error) => {
-//   if (
-//     error.response?.config & (error.response?.status === 401) ||
-//     error.response?.status === 403
-//   ) {
-//     store.dispatch('auth/logout')
-//     return error.response
-//   }
-//
-//   return Promise.reject(error)
-// })
+client.interceptors.response.use(null, (error) => {
+  if (
+    error.response?.config & (error.response?.status === 401) ||
+    error.response?.status === 403
+  ) {
+    //store.dispatch('auth/logout')
+    return error.response
+  }
+
+  return Promise.reject(error)
+})
 
 client.interceptors.request.use(
   (config) => {
